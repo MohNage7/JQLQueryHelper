144462362fd417ef9de88ead8fcb02a4b2af70c1
Mohamed Nageh <mohamed.nageh@swvl.com>
2021-04-10 16:49:21 +0200

    Change App name

--- src/mohnage7/Main.java
+++ src/mohnage7/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Pane root = FXMLLoader.load(getClass().getResource("name_fetcher_scene.fxml"));
-        primaryStage.setTitle("File Name Fetcher");
+        primaryStage.setTitle("JQL Helper");
         primaryStage.setScene(new Scene(root, 600, 560));
         primaryStage.setResizable(false);
         primaryStage.show();
